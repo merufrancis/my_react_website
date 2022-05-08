646f252bbafe0242be5f4ea200815595117a9971
merufrancis <64151444+merufrancis@users.noreply.github.com>
2022-05-08 13:59:32 +0300

    Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.css'
+import Button from './Button'
+
+const Navbar = () => {
+  return (
+    <div className='header'>
+        <h1>Techwiz</h1>
+        <ul>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+        </ul>
+        <Button />
+    </div>
+  )
+}
+
+export default Navbar
