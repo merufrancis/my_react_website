9958e7976fc4d14d002cbe091747e79e2842ccbc
merufrancis <64151444+merufrancis@users.noreply.github.com>
2022-05-11 15:44:51 +0300

    add responsiveness to the hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,8 +3,10 @@ import Button from './Button'
 
 const Hero = () => {
   return (
-    <div className='hero'>Hero
+    <div className='hero'>
+      <h3>Feel free to contact us <br></br> for our affordable services </h3>
     <div className='card'>
+      <form>
       <h3>Contact us</h3>
       <label>First Name:</label>
       <input type="text" placeholder='John Doe'></input>
@@ -12,7 +14,8 @@ const Hero = () => {
       <input type="text" placeholder='abc@gmail.com'></input>
       <label>Phone Number:</label>
       <input type="number" placeholder='07895345535'></input>
-      <Button style="background-color: blue;" />
+      <Button />
+      </form>
     </div>
     <Button />
     </div>
