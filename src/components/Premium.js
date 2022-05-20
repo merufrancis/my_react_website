e24ea6edfa7bce4ce7d1050657b68b6ab1049440
merufrancis <64151444+merufrancis@users.noreply.github.com>
2022-05-20 18:45:46 +0300

    Add Premium section

--- src/components/Premium.js
+++ src/components/Premium.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import './Premium';
+import React from 'react';
+import '../components/Premium.css';
 
 const premium = () => {
   return (
-    <div>
-        <h3>Premium Movies</h3>
+    <div className="prem">
+    <div className="less">
+        <h2>Premium Movies</h2>
+        <h4>Upcoming Movies</h4>
+    </div>
+    <div className="grid-container">
+      <div className="grid-item1">
+        <h3>Money Heist</h3>
+      </div>
+      <div className="grid-item2">
+      <h3>Merlin</h3>
+      </div>
+      <div className="grid-item3">
+      <h3>Bad Lands</h3>
+      </div>
+      <div className="grid-item4">
+      <h3>365 Days</h3>
+      </div>
+    </div>
     </div>
   )
 }
 
-export default premium
+export default premium;
